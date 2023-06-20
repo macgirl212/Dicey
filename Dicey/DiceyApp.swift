2c69c90d6c537f299918fa583c3fc134aa66c147
Melody Davis <piano.fan122@gmail.com>
2023-06-20 11:32:29 -0400

    Initial Commit

---
+++ Dicey/DiceyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceyApp.swift
+//  Dicey
+//
+//  Created by Melody Davis on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
